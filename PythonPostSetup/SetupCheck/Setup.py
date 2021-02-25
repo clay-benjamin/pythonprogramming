fa1a07128ec9e25b8bf8b89bb0226ef8bb6d2054
clay-benjamin <clay.benjamin.pro@gmail.com>
2021-02-25 22:59:44 +0100

    Upload to GH

---
+++ PythonPostSetup/SetupCheck/Setup.py
@@ -0,0 +1,23 @@
+'''
+Created on 12 Feb 2021
+
+@author: clayb
+'''
+
+def addTwoValues(a, b):
+    return a + b
+
+
+def addFixedValue(a):
+    y = 5
+    return y + a
+
+print("Return some values")
+
+ansTwoVal = addTwoValues(1, 2)
+print (ansTwoVal)
+
+ansFixedVal = addFixedValue(7)
+print(ansFixedVal)
+
+print("Github Check...")
